a76fe13ed69298ca0d32873ec7db89200179ab95
rauls19 <raul.lovi@gmail.com>
2021-02-28 17:55:03 +0100

    file removing cells + EDA - PCA explanations

---
+++ removeCellOutput.py
@@ -0,0 +1,23 @@
+"""
+Usage: python remove_output.py notebook.ipynb
+Python version: 3.9.X
+Author: rauls19
+"""
+import sys
+import io
+import os
+import nbformat as nbf
+
+def removeCellOutputs(data):
+    for cell in data.cells:
+        if cell.cell_type == 'code':
+            cell.outputs = []
+    return data
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    with open(file_name, 'r') as f:
+        nb = nbf.read(f, nbf.NO_CONVERT)
+    nb = removeCellOutputs(nb)
+    with open(file_name, "w") as f:
+        nbf.write(nb, f, nbf.NO_CONVERT)
