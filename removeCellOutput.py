760b22bc3aa6c5c3cc601c5dfdca58d179064ada
rauls19 <raul.lovi@gmail.com>
2021-03-28 03:39:56 +0200

    Removed prints removeCell.. + model finished LSTM, waiting for optimizations

--- removeCellOutput.py
+++ removeCellOutput.py
@@ -10,9 +10,7 @@ import nbformat as nbf
 
 def removeCellOutputs(data):
     for cell in data.cells:
-        print('Cell: ',cell)
         if cell.cell_type == 'code':
-            print('Esborrada')
             cell.outputs = []
     return data
 
@@ -21,7 +19,6 @@ if __name__ == '__main__':
     file_name = 'model.ipynb'
     with open(file_name, 'r', encoding='utf8') as f:
         nb = nbf.read(f, nbf.NO_CONVERT)
-    print('obert')
     nb = removeCellOutputs(nb)
     with open(file_name, 'w', encoding='utf8') as f:
         nbf.write(nb, f, nbf.NO_CONVERT)
